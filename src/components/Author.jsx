c89cd3a2d778a620f364d9fafcd6ecd5de79834b
dilnakt-bnglr <dilna.devaraj98@gmail.com>
2026-03-04 16:55:33 +0530

    Added router

---
+++ src/components/Author.jsx
@@ -0,0 +1,8 @@
+function Author() {
+  return (
+    <div>
+      <h1>Author Component</h1>
+    </div>
+  );
+}
+export default Author;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Author from "./components/Author.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/author",
+    element: <Author />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
